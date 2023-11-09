46800d72cc88d887330a03fdc605400b65fb89da
samurbanetto <101749449+samurbanetto@users.noreply.github.com>
2023-11-09 11:19:51 -0300

    beeboo

---
+++ trabalhoBD4/php/inserir_livro.php
@@ -0,0 +1,27 @@
+<?php
+    $dbconn = pg_connect("host=localhost port=5432 dbname=biblioteca user=postgres password=postgres");
+    if (!$dbconn) {
+    echo "Wasn't able to connect";
+    }
+
+    if (isset($_POST["submit"])){
+        if ($_POST["senha"] === $_POST["confsenha"]){                
+            $emails = "select email from usuarios";
+            $result = pg_query($dbconn, $emails);
+            $guidiimestre = false;
+
+            while ($row = pg_fetch_row($result)) {
+                if ($row[0] === $_POST["email"]) {
+                    $guidiimestre = true;
+                    break;
+                }
+            }
+
+            if(!$guidiimestre) {
+                $query = "insert into usuarios (nome, email, senha, tipo) values ('{$_POST["nome"]}', '{$_POST["email"]}',  '{$_POST["senha"]}',  {$_POST["tipo"]})";
+                pg_query($dbconn, $query);
+                header("Location: http://localhost:8080/index.php");
+            }
+        }
+    }
+?>
